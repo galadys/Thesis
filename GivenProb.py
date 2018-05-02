7d531afc3234bffcd1b9f6a5f102c16b98c5eda0
Lorenzo Meninato <lorenzomeninato@gmail.com>
2018-05-01 23:03:06 -0400

    adding plots

--- GivenProb.py
+++ GivenProb.py
@@ -2,10 +2,12 @@ import numpy as np
 from scipy.optimize import minimize
 from scipy.stats import  expon
 
-lambdaB = 1 #Buyer's exponential distribution parameter
-lambdaS = 1 #Seller's exponential distribution parameter
+lambdaB = 3 #Buyer's exponential distribution parameter
+lambdaS = 3 #Seller's exponential distribution parameter
 dist = 4
 s = 9
+numB = 1000
+numS = 600
 
 
 def CDF(x, parameter):
@@ -14,70 +16,64 @@ def CDF(x, parameter):
 def PDF(x, parameter):
     return expon.pdf(x, scale = 1/parameter)
 
-prob = [0.5, 0.5, 0.5]
-
 def objective(x):
-    r = x[6] =.1
+    r = x[6] #=.1
     PL = [x[0], x[1], x[2]]
     PH = [x[3], x[4], x[5]]
+    #prob = [x[7], x[8], x[9]]
     location = [0,1,2]
     output = 0
     for i in location:
-        output = output + r*(NL(x, i, prob)*PL[i] + NH(x, i, prob)*PH[i])
+        output = output + r*(NL(x, i)*PL[i] + NH(x, i)*PH[i])
     return -output
 
-def NH(x, location, prob):
-    return 1-CDF((x[3+location]-x[0+location])/(prob[location]), lambdaB)
+def NH(x, location):
+    return 1-CDF((x[3+location]-x[location])/(x[7+location]), lambdaB)
         
-def NL(x, location, prob):
-    return CDF((x[3+location]-x[0+location])/prob[location], lambdaB) - CDF((s-x[0+location])/(1+prob[location]), lambdaB)
+def NL(x, location):
+    return max(CDF((x[3+location]-x[location])/x[7+location], lambdaB) - CDF((s-x[location])/(1+x[7+location]), lambdaB), 0)
 
-def NS(x, location, prob):
-    return 1- CDF(
-            (NH(x, location, prob)*(x[3+location]-x[6])+NL(x, location, prob)*(x[location]-x[6]))
-            /(NH(x, location, prob) + (1-prob[location])*NL(x, location, prob)+2*prob[location]*NL(x, location, prob))
+def NS(x, location):
+    return  CDF(
+            ((NH(x, location)*x[3+location])*(1-x[6])+(NL(x, location)*x[location])*(1-x[6]))
+            /(NH(x, location) + (1-x[7+location])*NL(x, location)+2*x[7+location]*NL(x, location))
             , lambdaS)
 
 def constraintGenEq1(x, location = 0):
-    return NS(x, location, prob)- NH(x, location, prob) -(1+prob[location])*NL(x, location, prob)
+    return numS*NS(x, location)- numB*NH(x, location) -numB*(1-x[8]/2-x[9]/2)*NL(x, location)
 def constraintGenEq2(x, location = 1):
-    return NS(x, location, prob)- NH(x, location, prob) -(1+prob[location])*NL(x, location, prob)
-def constraintGenEq3(x, location = 2):
-    return NS(x, location, prob)- NH(x, location, prob) -(1+prob[location])*NL(x, location, prob)
+    return numS*NS(x, location)- numB*NH(x, location) -numB*(1-x[7]/2-x[9]/2)*NL(x, location)
+def constraintGenEq3(x,location = 2):
+    return numS*NS(x, location)- numB*NH(x, location) -numB*(1-x[7]/2-x[8]/2)*NL(x, location)
 
-def constraintProbLow1(x):
-    return x[7] - (x[3]-x[0])/(dist) 
-def constraintProbLow2(x):
-    return x[8] - (x[4]-x[1])/(dist) 
-def constraintProbLow3(x):
-    return x[9] - (x[5]-x[2])/(dist) 
 
-conGen1= {'type': 'ineq', 'fun': constraintGenEq1} 
-conGen2= {'type': 'ineq', 'fun': constraintGenEq2}
-conGen3= {'type': 'ineq', 'fun': constraintGenEq2}
-conProb1= {'type': 'ineq', 'fun': constraintProbLow1}
-conProb2= {'type': 'ineq', 'fun': constraintProbLow2}
-conProb3= {'type': 'ineq', 'fun': constraintProbLow3}
+conGen1= {'type': 'eq', 'fun': constraintGenEq1} 
+conGen2= {'type': 'eq', 'fun': constraintGenEq2}
+conGen3= {'type': 'eq', 'fun': constraintGenEq3}
 
-c1 = ([conGen1, conGen2, conGen3])
-cons = ([conGen1, conGen2, conGen3, conProb1, conProb2, conProb3])
-bnds1 = ((0,s),(0,s),(0,s),(0,s-dist),(0,s-dist),(0,s-dist),(0,1))
-bnds2 = ((0,s),(0,s),(0,s),(0,s),(0,s),(0,s),(0,1),(0,1),(0,1),(0,1))
+
+
+cons = ([conGen1, conGen2, conGen3])
+bnds1 = ((0,s-dist),(0,s-dist),(0,s-dist),(0,s-dist),(0,s-dist),(0,s-dist),(0,1),(0,1),(0,1),(0,1))
 
 n=len(bnds1)
 x0 = np.zeros(n)+0.1
 
-solution = minimize(objective, x0, bounds = bnds1, constraints = c1)
+solution = minimize(objective, x0, bounds = bnds1, constraints = cons, options = {'maxiter': 100})
 print(solution)
 np.round(solution.x, decimals = 2)
 
-parameterList = []
+parameterList = []#{'service':, 'distance':, 'initial':, 'bounds':, 'constraints':, 'solution':}
 
-def altminimize(objective, x0, bounds=bnds1, constraints=c1):
-    global parameterList
+def altminimize(objective, x0, bounds=bnds1, constraints=cons):
+    global parameterList, s, dist, prob
     solution = minimize(objective, x0, bounds=bounds, constraints=constraints)
-    parameterList.append( (x0, bounds, constraints, solution.x) )
+    parameterList.append( dict(probs= prob,service=s, distance= dist,initial =x0, bounds=bounds, constraints=constraints, solution=solution.x) )
     return print(solution)
 
-altminimize(objective, x0, bnds1, c1)
+altminimize(objective, x0, bnds1, cons)
 parameterList
+
+prob1 = (solution.x[3]-solution.x[0])/dist
+prob = [prob1, prob1, prob1]
+
--- symbolicMath.py
+++ symbolicMath.py
@@ -1,10 +1,70 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 27 19:50:58 2018
 
-@author: Marcelo
-"""
-from sympy import *
-init_printing(use_unicode=True)
+from GivenProb import CDF, PDF
+import numpy as np
+import matplotlib.pyplot as plt
 
-diff(sin(x)*exp(x), x)
+lambdaB = 3 #Buyer's exponential distribution parameter
+lambdaS = 3 #Seller's exponential distribution parameter
+dist = 4
+s = 9
+numB = 1000
+numS = 600
+numH = 1500
+numL = 2000
+r = .1
+ph = 5
+pl = 3
+x1 = .5
+l1 = 1
+
+X = np.linspace(0, 5,100, endpoint=True)
+
+numL = CDF((ph - X)/x1, lambdaB) - CDF((s-X)/(1+x1), lambdaB)
+pL = r*numL - r*X*(PDF((ph - X)/x1, lambdaB)-PDF((s-X)/(1+x1), lambdaB)) + r*ph*PDF((ph - X)/x1, lambdaB)
+
+ypLmax = max(pL)
+xpLmax = X[pL.argmax(axis=0)]
+fig = plt.figure()
+plt.plot(X, pL)
+fig.suptitle('Comparative Statics: Changes in Price')
+plt.xlabel('Price of Low-Quality Ride')
+plt.ylabel('Derivative of Firm\'s Profit')
+#plt.savefig('test.jpg')
+
+numH = 1-CDF((X-pl)/x1, lambdaB)
+pH = r*numH-r*X*(PDF((X-pl)/x1, lambdaB))+ r*pl*(PDF((X-pl)/x1, lambdaB))
+
+fig = plt.figure()
+plt.plot(X, pH)
+fig.suptitle('Comparative Statics: Changes in Price, with low-price=3')
+plt.xlabel('Price of High-Quality Ride')
+plt.ylabel('Derivative of Firm\'s Profit')
+#plt.savefig('test.jpg')
+
+X2 = np.linspace(0.01, 1,100, endpoint=False)
+
+prob1= r*pl*(-PDF((ph-pl)/X2,lambdaB)*(ph-pl)/pow(X2,2)+PDF((s-pl)/(1+X2), lambdaB)*(s-pl)/pow(1+X2,2))+r*ph*(PDF((ph-pl)/X2, lambdaB)*(ph-pl)/pow(X2,2))
+fig = plt.figure()
+plt.plot(X2, prob1)
+fig.suptitle('Comparative Statics: Changes in Pooling Probability')
+plt.xlabel('Pooling Probability')
+plt.ylabel('Derivative of Firm\'s Profit')
+
+
+
+X = np.linspace(0, 5,100, endpoint=True)
+
+x1=.5
+numL = CDF((ph - X)/x1, lambdaB) - CDF((s-X)/(1+x1), lambdaB)
+G1 = CDF((numH*(ph-r)+ numL*(X-r))/(numH + (1+x1)*numL), lambdaS)- CDF((ph-X)/x1, lambdaB) + (1-x1)*CDF((ph - X)/x1, lambdaB) - CDF((s-X)/(1+x1), lambdaB)
+dG1 = np.zeros(G1.shape, np.float)
+dG1[0:-1] = np.diff(G1)/np.diff(X)
+dG1[-1] = (G1[-1] - G1[-2])/(X[-1] - X[-2])
+pL2 = r*numL - r*X*(PDF((ph - X)/x1, lambdaB)-PDF((s-X)/(1+x1), lambdaB)) + r*ph*PDF((ph - X)/x1, lambdaB) -l1*(dG1)
+
+fig = plt.figure()
+plt.plot(X, pL2)
+fig.suptitle('Comparative Statics: Changes in Price')
+plt.xlabel('Price of Low-Quality Ride')
+plt.ylabel('Derivative of Firm\'s Profit')
